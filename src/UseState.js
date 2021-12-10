505d735b407390c8aadbbcb3e9541d6e56b2d88e
danielpqe <danielpqe@gmail.com>
2021-12-09 21:41:21 -0500

    initial commit

---
+++ src/UseState.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function UseState() {
+    return (
+        <div>
+                <h2>Eliminar UseState</h2>
+                <p>Ingrese el código de seguridad</p>
+                <input type="text" placeholder="Código de seguridad" />
+                <button>Comprobar</button>
+            </div>
+    )
+}
+
+export { UseState }
